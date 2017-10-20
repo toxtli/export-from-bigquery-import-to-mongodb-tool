9e38465918b4f905c38c49359da44a0224e07a71
Carlos Toxtli <ctoxtli@gmail.com>
2017-10-20 16:50:19 -0400

    Dynamic headers

--- query.py
+++ query.py
@@ -1,23 +1,18 @@
-# python query.py 'SELECT * FROM [fh-bigquery:reddit_comments.2016_07] WHERE subreddit="The_Donald" AND author <> "[deleted]"' 
-
-import argparse, uuid, pymongo
+import config, argparse, uuid, pymongo
 from google.cloud import bigquery
 
-conn = pymongo.MongoClient()["test"]["trump"]
-headers = ["body","score_hidden","archived","name","author","author_flair_text","downs","created_utc","subreddit_id","link_id","parent_id","score","retrieved_on","controversiality","gilded","id","subreddit","ups","distinguished","author_flair_css_class"]
-
 def query(query):
+    conn = pymongo.MongoClient()[config.db][config.table]
     client = bigquery.Client()
     query_job = client.run_async_query(str(uuid.uuid4()), query)
     query_job.begin()
     query_job.result()
     destination_table = query_job.destination
     destination_table.reload()
+    headers = [field.name for field in destination_table.schema]
     cont = 0
     for row in destination_table.fetch_data():
-        record = dict(zip(headers, row))
-        record["_id"] = str(record["created_utc"]) + str(record["link_id"])
-        conn.save(record)
+        conn.save(dict(zip(headers, row)))
         cont += 1
         print(cont)
 
